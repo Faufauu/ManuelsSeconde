516ee80c5763d58dfa485f706426a428d9d661ec
Faufauu <faufau.smits@gmail.com>
2024-09-10 20:00:08 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 export const firebaseConfig = {
     apiKey: "AIzaSyA_o5fz_3ELok5uTDgJfIhL7hzqv4-US6I",
     authDomain: "manuelsseconde.firebaseapp.com",
@@ -6,5 +5,6 @@ export const firebaseConfig = {
     projectId: "manuelsseconde",
     storageBucket: "manuelsseconde.appspot.com",
     messagingSenderId: "369461165606",
-    appId: "1:369461165606:web:6bd9186ee95e3701d3f0e7"
+    appId: "1:369461165606:web:6bd9186ee95e3701d3f0e7",
+    measurementId: "G-W508347RVD" // Incluez-le uniquement si vous utilisez les Analytics
 };
