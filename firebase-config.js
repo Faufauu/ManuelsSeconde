12e8a0fdbd68d3a7a5a520b409e5e843d8c4c836
Faufauu <faufau.smits@gmail.com>
2024-09-10 19:13:42 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyA_o5fz_3ELok5uTDgJfIhL7hzqv4-US6I",
+    authDomain: "manuelsseconde.firebaseapp.com",
+    databaseURL: "https://manuelsseconde.firebaseio.com",
+    projectId: "manuelsseconde",
+    storageBucket: "manuelsseconde.appspot.com",
+    messagingSenderId: "369461165606",
+    appId: "1:369461165606:web:6bd9186ee95e3701d3f0e7"
+};
