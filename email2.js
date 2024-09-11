fe66dae649e81165c00fdbd34d5e2f99f0aa56de
Faufauu <faufau.smits@gmail.com>
2024-09-11 15:34:56 +0200

    Add files via upload

---
+++ email2.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_8lehz4x";
+  const templateID = "template_xxgdgj3";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Message Envoyé.")
+
+    })
+    .catch(err=>console.log(err));
+
+}
